60df1b93659bca950174f81d2a3a5f3ed8c349e7
Mark Schiltz <maschiltz@gmail.com>
2015-02-23 21:51:23 -0500

    adding data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-
+    url = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/BullSchlitz79?api_key=b75dd34f-46a6-44b8-9633-35a95fc51fec'
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    puts 'dddddddd'
+    puts res.body
+    @data = res.body
   end
 end
