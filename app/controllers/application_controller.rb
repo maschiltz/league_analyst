c5a80697d47fd43561dfddbbd14b23913f1fe0ef
maschiltz <maschiltz@gmail.com>
2015-02-23 23:26:48 -0500

    crap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,22 @@ class ApplicationController < ActionController::Base
 
   def index
     summoner_name = 'BullSchlitz79'
-    url = URI.parse("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/#{summoner_name}?api_key=b75dd34f-46a6-44b8-9633-35a95fc51fec")
-    req = Net::HTTP::Get.new(url.to_s)
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    response = http.request(req)
-    summoner_base = JSON.parse(response.body)
-   
-    summoner_id = summoner_base[summoner_name.downcase]['id']
-
-    url = URI.parse("https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/#{summoner_id}/recent?api_key=b75dd34f-46a6-44b8-9633-35a95fc51fec")
-    req = Net::HTTP::Get.new(url.to_s)
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    response = http.request(req)
-
-    @data =  response.body
+#    url = URI.parse("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/#{summoner_name}?api_key=b75dd34f-46a6-44b8-9633-35a95fc51fec")
+#    req = Net::HTTP::Get.new(url.to_s)
+#    http = Net::HTTP.new(url.host, url.port)
+#    http.use_ssl = true
+#    response = http.request(req)
+#    summoner_base = JSON.parse(response.body)
+#   
+#    summoner_id = summoner_base[summoner_name.downcase]['id']
+#
+#    url = URI.parse("https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/#{summoner_id}/recent?api_key=b75dd34f-46a6-44b8-9633-35a95fc51fec")
+#    req = Net::HTTP::Get.new(url.to_s)
+#    http = Net::HTTP.new(url.host, url.port)
+#    http.use_ssl = true
+#    response = http.request(req)
+#
+#    @data =  response.body
+    @data = 'damn'
   end
 end
